29d9f67c09a37e04e3421c24a3c4a5b82a4134a3
Dinesh Nanda <dannynanda76@gmail.com>
2020-03-05 17:20:53 -0500

    Second smallest number in array

---
+++ Second smallest number~/SecondSmallestInArray/src/main/java/second_Smallest/SecondSmallestInArray.java
@@ -0,0 +1,31 @@
+package second_Smallest;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+public class SecondSmallestInArray {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 5, 6, 3, 2};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 1) {
+                System.out.print("Second smallest number in array is: "+arr[i]);
+                break;
+            }
+        }
+    }
+
+}
